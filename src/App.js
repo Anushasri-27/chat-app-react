ee624dade69fd647d6947f4f8f89a61b408a4145
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-09-23 00:55:37 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import '../src/styles/main.scss';
+import {Switch  } from 'react-router';
+import SignIn from './pages/SignIn';
+import PrivateRoute from '../src/components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-     hello
-     kjadbadkjc
-     hbdhbd
-    </div>
+    <Switch>
+        <PublicRoute path="/signin">
+              <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+              <Home /> 
+        </PrivateRoute>
+    </Switch>
   );
 }
 
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,40 +1,18 @@
-import { Notification as Toast } from 'rsuite';
+import firebase from 'firebase/app';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import { getStorage } from 'firebase/storage';
-import { getMessaging, isSupported, onMessage } from 'firebase/messaging';
-import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
-import { isLocalhost } from './helpers';
+
+
 
 const config = {
-  apiKey: 'AIzaSyAKGRssxY4OW9um6RlSbbQ1Fh_lgdWfoPs',
-  authDomain: 'chat-web-app-4ee4c.firebaseapp.com',
-  databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
-  projectId: 'chat-web-app-4ee4c',
-  storageBucket: 'chat-web-app-4ee4c.appspot.com',
-  messagingSenderId: '167319830934',
-  appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
+  apiKey: "AIzaSyBeBZsmKqrrfqm09Ljo-dCHMGQosZnx-oI",
+  authDomain: "chat-web-app-d7e15.firebaseapp.com",
+  databaseURL: "https://chat-web-app-d7e15-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-d7e15",
+  storageBucket: "chat-web-app-d7e15.appspot.com",
+  messagingSenderId: "810768504740",
+  appId: "1:810768504740:web:eaa0bea83008b8705a5331"
 };
 
-export const fcmVapidKey =
-  'BLs_I-HQyrAuUJJh8H3U0vtHGhVhXLMqoVoomeNL90GMKm0-o7sSoN9CJYRiBAVz-Yi7ZAni8mKateJfDwodTnw';
-
 const app = initializeApp(config);
-export const auth = getAuth(app);
-export const database = getDatabase(app);
-export const storage = getStorage(app);
-export const functions = getFunctions(app, 'europe-west3');
 
-export const messaging = isSupported() ? getMessaging(app) : null;
 
-if (messaging) {
-  onMessage(messaging, ({ notification }) => {
-    const { title, body } = notification;
-    Toast.info({ title, description: body, duration: 0 });
-  });
-}
-
-if (isLocalhost) {
-  connectFunctionsEmulator(functions, 'localhost', 5001);
-}
