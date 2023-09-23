b1cc3df4617e5e4fccf2afc5aeb908e825d91e43
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-09-24 02:43:11 +0530

    manage user with profilr context

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,14 +1,55 @@
-import React, { createContext, useContext, useState } from 'react';
+import  { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { auth ,database} from '../misc/firebase';
 
 const ProfileContext = createContext();
 
 //providor that will provide profilecontext to all its children
 
 export const ProfileProvider = ({ children }) => {
-  const [profile, setProfiles] = useState(false);
+  const [profile, setProfile] = useState(null);
+  const [isLoading ,setIsLoading] =useState(true);
+  //using useEffect to get user from firebase when component mount
+  useEffect(() => {
+    let userRef;
 
-  return <ProfileContext.Provider value={profile}>{children}</ProfileContext.Provider>;
+  const authUnsub =  auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef=database.ref(`/profiles/${authObj.uid}`);
+        userRef.on('value', snap => {
+          const { name, createdAt } = snap.val();
+
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+          setProfile(data);
+          setIsLoading(false);
+        });
+      } else {
+        if(userRef){
+          userRef.off();
+        }
+        setProfile(null);
+        setIsLoading(true);
+      }
+    });
+
+    //cleanUP function --->when component unmount
+    return () =>{
+      authUnsub();
+      if(userRef){
+        userRef.off();
+      }
+    }
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={{profile, isLoading}}>
+      {children}
+    </ProfileContext.Provider>
+  );
 };
 
-
-export const useProfile =  () => useContext(ProfileContext);
+export const useProfile = () => useContext(ProfileContext);
