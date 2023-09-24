ac63514de9871d2ac93de30593ae56300a5f7c84
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-09-25 00:54:39 +0530

    updated nickname in database onChange

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,35 +1,41 @@
-import { Button, Divider, Drawer } from 'rsuite';
-import {useProfile} from '../../context/profile.context'
+import { Alert, Button, Divider, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 import EditableInput from './EditableInput';
+import { database } from '../../misc/firebase';
 
-const Dashboard = ({onSignOut}) => {
-  const {profile} =useProfile();
-  const onSave = async (newData) =>{
-    console.log(newData)
-
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+  const onSave = async newData => {
+    //database.ref(`/profiles/${profile.uid/name}`) --->wil also work
+    const userNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+    try {
+      await userNickNameRef.set(newData);
+      Alert.success('Nickname Has Been Updated', 4000);
+    } catch (err) {
+      Alert.error(err.message,4000);
+    }
   };
   return (
     <>
       <Drawer.Header>
-        <Drawer.Title>
-           Dashboard
-        </Drawer.Title>
+        <Drawer.Title>Dashboard</Drawer.Title>
       </Drawer.Header>
       <Drawer.Body>
-         <h3>Hey ,{profile.name}</h3>
+        <h3>Hey ,{profile.name}</h3>
         <Divider />
         <EditableInput
-           name="nickname"
-           initialValue ={profile.name}
-           onSave={onSave}
-           label={<h6 className='mb-2'>Nickname</h6>}
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
         />
-        
       </Drawer.Body>
-      <Drawer.Footer> 
-           <Button block color="violet"  onClick={onSignOut}>
-               sign out
-           </Button>
+      <Drawer.Footer>
+        <Button block color="violet" onClick={onSignOut}>
+          sign out
+        </Button>
       </Drawer.Footer>
     </>
   );
