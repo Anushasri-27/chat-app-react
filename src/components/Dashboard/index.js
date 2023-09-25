1e979a1d940d710ad66dbe1b440a23c50f59ffce
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-09-25 17:32:22 +0530

    started to work with user avatar upload

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,7 +1,9 @@
 import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
-import EditableInput from './EditableInput';
+import EditableInput from '../EditableInput';
 import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
+import AvatarUploadBtn from './AvatarUploadBtn';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
@@ -24,6 +26,7 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey ,{profile.name}</h3>
+        <ProviderBlock />
         <Divider />
         <EditableInput
           name="nickname"
@@ -31,10 +34,11 @@ const Dashboard = ({ onSignOut }) => {
           onSave={onSave}
           label={<h6 className="mb-2">Nickname</h6>}
         />
+        <AvatarUploadBtn />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="violet" onClick={onSignOut}>
-          sign out
+          LOGOUT
         </Button>
       </Drawer.Footer>
     </>
--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect  } from "react";
 
 export function useModalState (daefaultValue =false){
 
